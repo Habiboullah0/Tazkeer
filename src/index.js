c965de4c2d04d51b3f4bdb7ab39c28533ae0009f
Habiboullah0 <habiboullahmedlemin7@gmail.com>
2025-05-19 22:32:43 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './css/tailwind.css';
+import TazkeerApp from './TazkeerApp';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <TazkeerApp />
+  </React.StrictMode>
+);
